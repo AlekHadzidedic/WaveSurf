00af5dba239e9d9e95f1d04824067bb89e154a64
Alek Hadzidedic <alek.hadzidedic@gmail.com>
2019-04-11 20:26:27 -0400

    finished thesaurus

--- Setup.py
+++ Setup.py
@@ -3,6 +3,7 @@ import HTMLParser
 import SGMLParser
 import DictionaryBuilder
 import InvertedIndexBuilder
+import ThesaurusBuilder
 import os
 
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
@@ -17,6 +18,7 @@ doc_url = os.path.join(SITE_ROOT, "static/data/", "documents.json")
 doc_reuters_url = os.path.join(SITE_ROOT, "static/data/", "documents_reuters.json")
 inverted_index_url = os.path.join(SITE_ROOT, "static/data/", "inverted_index.json")
 inverted_index_reuters_url = os.path.join(SITE_ROOT, "static/data/", "inverted_index_reuters.json")
+thesaurus_reuters = os.path.join(SITE_ROOT, "static/data/", "thesaurus_index.json")
 
 # p = HTMLParser.MyHTMLParser()
 # stringHtml = open(raw_doc_url, 'r').read()
@@ -33,15 +35,17 @@ inverted_index_reuters_url = os.path.join(SITE_ROOT, "static/data/", "inverted_i
 # index = InvertedIndexBuilder.create_inverted_index(json.load(open(dict_url)), json.load(open(tokenized_doc_url)))
 # json.dump([index], open(inverted_index_url, 'w'), indent=2)
 
-s = SGMLParser.MySGMLParser()
-string_sgml = open(raw_doc_reuters_url, 'r').read()
-s.feed(string_sgml)
+# s = SGMLParser.MySGMLParser()
+# string_sgml = open(raw_doc_reuters_url, 'r').read()
+# s.feed(string_sgml)
+#
+# json.dump([s.documents], open(doc_reuters_url, 'w'), indent=2)
+#
+# dictionary_reuters = DictionaryBuilder.create_dictionary(True, True, True, json.load(open(doc_reuters_url)))
+# json.dump(DictionaryBuilder.tokenized_documents, open(tokenized_doc_reuters_url, 'w'), indent=2)
+# json.dump([dictionary_reuters], open(dict_reuters_url, 'w'), indent=2)
+# index = InvertedIndexBuilder.create_inverted_index(json.load(open(dict_reuters_url)),
+#                                                    json.load(open(tokenized_doc_reuters_url)))
+# json.dump([index], open(inverted_index_reuters_url, 'w'), indent=2)
 
-json.dump([s.documents], open(doc_reuters_url, 'w'), indent=2)
-
-dictionary_reuters = DictionaryBuilder.create_dictionary(True, True, True, json.load(open(doc_reuters_url)))
-json.dump(DictionaryBuilder.tokenized_documents, open(tokenized_doc_reuters_url, 'w'), indent=2)
-json.dump([dictionary_reuters], open(dict_reuters_url, 'w'), indent=2)
-index = InvertedIndexBuilder.create_inverted_index(json.load(open(dict_reuters_url)),
-                                                   json.load(open(tokenized_doc_reuters_url)))
-json.dump([index], open(inverted_index_reuters_url, 'w'), indent=2)
+ThesaurusBuilder.create_thesaurus()
--- ThesaurusBuilder.py
+++ ThesaurusBuilder.py
@@ -3,27 +3,28 @@ import os
 
 
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-json_url = os.path.join(SITE_ROOT, "static/data/", "inverted_index_reuters.json")
-dict_url = os.path.join(SITE_ROOT, "static/data/", "dictionary_reuters.json")
+json_url = os.path.join(SITE_ROOT, 'static/data/', 'inverted_index_reuters.json')
+dict_url = os.path.join(SITE_ROOT, 'static/data/', 'dictionary_reuters.json')
+thesaurus_reuters = os.path.join(SITE_ROOT, 'static/data/', 'thesaurus_index.json')
 
 docs = json.load(open(json_url))
 dictionary = json.load(open(dict_url))
 
-thesaurus = []
+thesaurus = {}
 
 
 def doc_id_to_list(dict_list):
     id_list = []
 
     for elem in dict_list:
-        id_list.append(elem["DocID"])
+        id_list.append(elem['DocID'])
 
     return id_list
 
 
 def find_document(dict_list, doc_id):
     for doc in dict_list:
-        if doc["DocID"] == doc_id:
+        if doc['DocID'] == doc_id:
             return doc
 
     return {}
@@ -31,21 +32,39 @@ def find_document(dict_list, doc_id):
 
 def create_thesaurus():
     for word, doc_list in docs[0].items():
+        if len(thesaurus.keys()) >= 200:
+            json.dump(thesaurus, open(thesaurus_reuters, 'a'), indent=2)
+            thesaurus.clear()
 
+        thesaurus[word] = []
         doc_list_count = 0
 
-        for doc in doc_list:
-            doc_list_count +
+        for doc in doc_list['Postings']:
+            doc_list_count += doc['Term Frequency']
 
         for rel_word, rel_doc_list in docs[0].items():
-            if word != rel_word:
-                intersection = list(set(doc_id_to_list(doc_list)) & set(doc_id_to_list(rel_doc_list)))
-                sim_counter = 0
-                total_counter = 0
+            if word != rel_word and rel_word not in thesaurus.keys():
+                intersection = list(set(doc_id_to_list(doc_list['Postings'])) & set(doc_id_to_list(rel_doc_list['Postings'])))
+                intersect_counter = 0
+                doc2_counter = 0
 
                 for doc_id in intersection:
-                    doc1 = find_document(doc_list, doc_id)
-                    doc2 = find_document(rel_doc_list, doc_id)
+                    doc1 = find_document(doc_list['Postings'], doc_id)
+                    doc2 = find_document(rel_doc_list['Postings'], doc_id)
 
-                    sim_counter += min(doc1["Term Frequency"], doc2["Term Frequency"])
+                    intersect_counter += min(doc1['Term Frequency'], doc2['Term Frequency'])
+
+                for rel_doc in rel_doc_list['Postings']:
+                    doc2_counter += rel_doc['Term Frequency']
+
+                jaccard = (intersect_counter*2)/(doc_list_count + doc2_counter)
+
+                if jaccard != 0:
+                    thesaurus[word].append({'word': rel_word, 'value': jaccard})
+
+
+        thesaurus[word] = sorted(thesaurus[word], key=lambda k: k['value'], reverse=True)
+
+        if len(thesaurus[word]) > 20:
+            thesaurus[word] = thesaurus[word][:20]
 
